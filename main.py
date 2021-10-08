869b460ea1cd830bfe750fad3bffdf76756f675c
NIKHIL JI <taparianikhil13@gmail.com>
2021-10-09 01:07:35 +0530

    new commit

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
-print ("Hello World")
+
+
+
+
+
+# import datetime
+# c = ord('\n')
+# today = datetime.datetime.today()
+# print(f'game {c} {today:%b %d %y}')
+
+# val = 4
+
+# can = 'dd'
+
+# print(f'i {can} be {val}')
+
+
+
+
+
+
+
+# import time
+
+# count =3
+
+# for i in reversed(range(count +1)):
+#     if i > 0:
+#         print(i,end=('>>'), flush=True)
+#         time.sleep(1)
+#     else:
+#         print('start')
+    
+
+
+
+
+
+# x,y = [int(x) for x in input('Enter Number : ').split()]
+
+# print('The value of x is {} and value of y is {}.'.format(x,y))
+
+
+
+
+# # print ("Hello World")
+
+# val = 500
+
+# def gamer():
+#     return val
+
+
+# var = int(input("Find the Number :"))
+
+# if var == val:
+#     print ('You won')
+# else:
+#     print('Losee')
+
+# print(type(var))
